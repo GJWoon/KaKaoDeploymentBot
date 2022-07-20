c9181d38216d4af3e669078462c28a8b163b8bc9
GJWoon <guemjiwoon@gmail.com>
2022-07-20 14:42:16 +0900

    First Commit

---
+++ src/main/java/com/example/kakaodeploymentbot/KaKaoDeploymentBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.kakaodeploymentbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaKaoDeploymentBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaKaoDeploymentBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kakaodeploymentbot/KaKaoDeploymentBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kakaodeploymentbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaKaoDeploymentBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
